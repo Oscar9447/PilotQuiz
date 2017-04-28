9df3047ca279df043a29b145af0325b73af1a9ad
Oscar9447 <Monday28>
2017-04-28 13:23:52 +0100

    UNI Upload

---
+++ app/src/main/java/com/example/oscar/pilotquiz/QuizActivity.java
@@ -0,0 +1,219 @@
+package com.example.oscar.pilotquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private TextView mScoreView;
+    private TextView mQuestion;
+
+    private Button mButtonChoice1, mButtonChoice2, mButtonChoice3, mButtonChoice4, mButtonQuit;
+
+    private int mScore = 0;
+    private int mQuestionNumber = 0;
+    private String mAnswer;
+
+
+    private DatabaseReference mQuestionRef, mChoice1Ref, mChoice2Ref, mChoice3Ref, mChoice4Ref, mAnswerRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mScoreView = (TextView)findViewById(R.id.score);
+        mQuestion = (TextView) findViewById(R.id.question);
+
+        mButtonChoice1 = (Button) findViewById(R.id.choice1);
+        mButtonChoice2 = (Button) findViewById(R.id.choice2);
+        mButtonChoice3 = (Button) findViewById(R.id.choice3);
+        mButtonChoice4 = (Button) findViewById(R.id.choice4);
+        mButtonQuit =  (Button) findViewById(R.id.quit);
+
+        updateQuestion();
+
+        //Button 1
+        mButtonChoice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mButtonChoice1.getText().equals(mAnswer)){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                }else{
+                    updateQuestion();
+                }
+            }
+        });
+        //Button1
+
+
+        //Button 2
+        mButtonChoice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mButtonChoice2.getText().equals(mAnswer)){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                }else{
+                    updateQuestion();
+                }
+            }
+        });
+        //Button2
+
+        //Button 3
+        mButtonChoice3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mButtonChoice3.getText().equals(mAnswer)){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                }else{
+                    updateQuestion();
+                }
+            }
+        });
+        //Button3
+
+
+        //Button 4
+        mButtonChoice4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mButtonChoice4.getText().equals(mAnswer)){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                }else{
+                    updateQuestion();
+                }
+            }
+        });
+        //Button4
+
+        //Quit Button
+        mButtonQuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+
+
+
+
+
+
+
+
+    }
+
+    private void updateScore(int score){
+        mScoreView.setText("" + mScore);
+    }
+
+    private void updateQuestion() {
+    mQuestionRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://pilotquiz.firebaseio.com/"+ mQuestionNumber +"/question");
+
+    mQuestionRef.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            String question = dataSnapshot.getValue(String.class);
+            mQuestion.setText(question);
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    });
+        mChoice1Ref = FirebaseDatabase.getInstance().getReferenceFromUrl("https://pilotquiz.firebaseio.com/"+ mQuestionNumber +"/choice1");
+        mChoice1Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String choice = dataSnapshot.getValue(String.class);
+                mButtonChoice1.setText(choice);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        mChoice2Ref = FirebaseDatabase.getInstance().getReferenceFromUrl("https://pilotquiz.firebaseio.com/"+ mQuestionNumber +"/choice2");
+        mChoice2Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String choice = dataSnapshot.getValue(String.class);
+                mButtonChoice2.setText(choice);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        mChoice3Ref = FirebaseDatabase.getInstance().getReferenceFromUrl("https://pilotquiz.firebaseio.com/"+ mQuestionNumber +"/choice3");
+        mChoice3Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String choice = dataSnapshot.getValue(String.class);
+                mButtonChoice3.setText(choice);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        mChoice4Ref = FirebaseDatabase.getInstance().getReferenceFromUrl("https://pilotquiz.firebaseio.com/"+ mQuestionNumber +"/choice4");
+        mChoice4Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String choice = dataSnapshot.getValue(String.class);
+                mButtonChoice4.setText(choice);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mAnswerRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://pilotquiz.firebaseio.com/"+ mQuestionNumber +"/answer");
+        mAnswerRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mAnswer = dataSnapshot.getValue(String.class);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mQuestionNumber ++;
+
+
+
+    }
+
+
+}
